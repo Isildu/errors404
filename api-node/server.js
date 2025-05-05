4af468514c8c6fcabe4dfc19598d6ba76ab43cce
polsalvador <pol.salvadori.7e8@itb.cat>
2025-05-05 21:53:02 +0200

    Node.js fet

--- api-node/server.js
+++ api-node/server.js
@@ -1,21 +1,42 @@
+// Obrir servidor: node server.js 
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Configuració per servir arxius estàtics (CSS, JS, imatges)
-app.use(express.static(path.join(__dirname, '../'))); // Puja 1 nivell des de `api-node`
+app.use(express.static(path.join(__dirname, '../')));
 
-// Ruta principal: ObsidianVogue.html
+// Ruta principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../ObsidianVogue.html')); // Puja 1 nivell ✅
+  res.sendFile(path.resolve(__dirname, '../ObsidianVogue.html'));
+    // http://localhost:3000/ObsidianVogue.html	
 });
 
-// Ruta per a Contacte
+// Contacte
 app.get('/contacte', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Contacto-ObsidianVogue.html'));
+  res.sendFile(path.resolve(__dirname, '/Contacto-ObsidianVogue.html'));
+    // http://localhost:3000//Contacto%E2%80%93ObsidianVogue.html
+});
+
+// Productes
+app.get('/productes', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../Productos-ObsidianVogue.html'));
+    // http://localhost:3000//Productos%E2%80%93ObsidianVogue.html
+});
+
+// Registre
+app.get('/registre', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../Registro-ObsidianVogue.html'));
+    // http://localhost:3000//Registro%E2%80%93ObsidianVogue.html
+});
+
+// Sobre Nosaltres (amb espai)
+app.get('/sobre-nosaltres', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../Sobre Nosotros-ObsidianVogue.html'));
+    // http://localhost:3000//Sobre%20Nosotros%E2%80%93ObsidianVogue.html
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionant a http://localhost:${PORT}`);
-});
+}); 
